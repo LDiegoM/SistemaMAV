ea6337d2e1782b31f7a9fd8b00bdcdfe7c9d5ca6
Diego M Lopez <diegol2k@msn.com>
2019-11-04 21:51:56 -0300

    Adapted MarcaViewModel and MarcaController

--- UI/Web/Models/MarcaViewModel.cs
+++ UI/Web/Models/MarcaViewModel.cs
@@ -1,15 +1,34 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using SistemaMAV.Entities.Models;
 
 namespace SistemaMAV.UI.Web.Models {
     public class MarcaViewModel {
+
         [Display(Name = "Código")]
         public int MarcaId { get; set; }
 
         [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "Debe ingresar el Nombre")]
         public string Detalle { get; set; }
         
         [Display(Name = "Activa?")]
         public bool Activo { get; set; }
+
+        public MarcaViewModel() {}
+
+        public MarcaViewModel(Marca marca) {
+            MarcaId = marca.MarcaId;
+            Detalle = marca.Detalle;
+            Activo = marca.Activo;
+        }
+
+        public Marca ToMarca() {
+            return new Marca() {
+                MarcaId = MarcaId,
+                Detalle = Detalle,
+                Activo = Activo
+            };
+        }
     }
 }
